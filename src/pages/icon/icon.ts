bd4b14a241a12292c1ca402f342b25ca1b57ede0
Guille <info@guillegonzarod.com>
2018-11-02 19:29:34 +0100

    Adición de las listas: 'Icon', 'Avatar', 'Multi-line', 'Sliding' y 'Thumbnail'.

--- src/pages/icon/icon.ts
+++ src/pages/icon/icon.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+// Importaciones:
+import { IContacts } from '../../models/data-source.model';
+import { GetContactsProvider } from '../../providers/get-contacts/get-contacts';
 
 /**
  * Generated class for the IconPage page.
@@ -15,11 +18,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class IconPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  //Propiedades:
+  contactos: IContacts[];
+
+  // Constructores:
+  constructor(public navCtrl: NavController, public navParams: NavParams, public getContactsProvider: GetContactsProvider) {
+    this.contactos = this.getContactsProvider.dataS;
   }
 
+  // Métodos:
   ionViewDidLoad() {
     console.log('ionViewDidLoad IconPage');
   }
 
+  itemSelected(item: IContacts) {
+    let mensaje = `Cliente: ${item.codigoCliente} seleccionado`;
+    this.getContactsProvider.showToast('middle', mensaje);
+  }
 }
